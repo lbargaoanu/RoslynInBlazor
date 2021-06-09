4e20f198e4cb6f803daf8f96fe4386f3890fb57b
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2021-06-09 10:34:38 +0300

    p

---
+++ RoslynInBlazor/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+namespace RoslynInBlazor.Pages
+{
+    public partial class Index
+    {
+        static Index()
+        {
+            System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.ConsoleTraceListener());
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            object result = await CSharpScript.EvaluateAsync("1 + 2");
+        }
+    }
+}
