80ca51c2328e10a89388193d5c9d2deabb26750c
Lucian Bargaoanu <lbargaoanu@users.noreply.github.com>
2021-08-15 08:17:58 +0300

    p

--- RoslynInBlazor/Pages/Index.razor.cs
+++ RoslynInBlazor/Pages/Index.razor.cs
@@ -60,7 +60,7 @@ namespace RoslynInBlazor.Pages
                  !_loadedAssemblies.ContainsKey(a.Name)).Select(async assemblyItem =>
                  {
                      Console.WriteLine("Loading " + assemblyItem.Name);
-                     var stream = await HttpClient.GetStreamAsync(Path.Combine("_framework", Path.GetFileName(assemblyItem.Location)));
+                     var stream = await HttpClient.GetStreamAsync(Path.Combine("_framework", assemblyItem.Name + ".dll"));
                      Console.WriteLine("Loaded " + assemblyItem.Name);
                      _loadedAssemblies.Add(assemblyItem.Name, MetadataReference.CreateFromStream(stream));
                  }));
